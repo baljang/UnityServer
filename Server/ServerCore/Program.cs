820ccd60bc80c93b85e0200141662f6b83500bd5
baljang <baljang7@gmail.com>
2022-10-17 10:59:19 +0900

    01_04_멀티쓰레드_캐시이론
    Q1. 코어가 여러개면 어떤 문제가 발생하는지 식당에서 음료수 주문을 번복하는 것에 빗대어 설명해 보세요.
    
    Q2. 무엇을 캐시할 것이냐에 대한 캐시철학 두 가지를 식당에서 일어나는 일에 빗대어 설명해 보세요.
    
    Q3. arr[10000][10000] 배열에 1을 arr[x,y] 순서로 넣는 것과, arr[y,x] 순서로 넣는 것과 속도가 다른 이유가 무엇인지 설명해 보세요.

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -6,35 +6,26 @@ namespace ServerCore
 {
     internal class Program
     {
-        volatile static bool _stop = false;  // 전역은 모든 스레드들이 동시 접근 가능. 스레드들이 동시 접근할 때 어떤 일이 일어날지 살펴본다.
-
-        static void ThreadMain()
-        {
-            Console.WriteLine("쓰레드 시작!");
-
-            while(_stop == false)
-            {
-                // 누군가가 stop 신호를 해주기를 기다린다
-            }
-
-            Console.WriteLine("쓰레드 종료!");
-        }
-
         static void Main(string[] args)
         {
-            Task t = new Task(ThreadMain);
-            t.Start();
+            int[,] arr = new int[10000, 10000];
+            {
+                long now = DateTime.Now.Ticks; 
+                for(int y =0; y<10000; y++)
+                    for(int x=0; x<10000; x++)
+                       arr[y, x] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(y,x) 순서 걸린 시가 {end - now}"); 
+            }
 
-            Thread.Sleep(1000); // 1초 동안 잠들었다 다시 깨는 함수
-
-            _stop = true;
-
-            Console.WriteLine("Stop 호출");
-            Console.WriteLine("종료 대기중");
-
-            t.Wait();   // 스레드가 끝났는지 알아 보는 함수. Thread일 때는 Join, Task는 Wait
-
-            Console.WriteLine("종료 성공");
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                        arr[x, y] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(x,y) 순서 걸린 시가 {end - now}");
+            }
         }
     }
 }
