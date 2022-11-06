f32bdb652d9fe043dbc1e26bf10d1303f3663088
baljang <baljang7@gmail.com>
2022-11-06 11:05:55 +0900

    01_09_멀티쓰레드_SpinLock
    Q1. 락을 구현할 때 Interlocked.CompareExchange를 이용하는 이유는 무엇인가?

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -4,19 +4,45 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
+    class SpinLock
+    {
+        volatile int _locked = 0;
+
+
+        public void Acquire()
+        {
+            while (true)
+            {
+                //int original = Interlocked.Exchange(ref _locked, 1);
+                //if (original == 0)
+                //    break;
+
+                // CAS Compare-And-Swap
+                int expected = 0;
+                int desired = 1;
+                if (Interlocked.CompareExchange(ref _locked, desired, expected) == expected)
+                    break;
+            }
+        }
+
+        public void Release()
+        {
+            _locked = 0;
+        }
+    }
+
     internal class Program
     {
-        static int number = 0;
-        static object _obj = new object(); 
+        static int _num = 0;
+        static SpinLock _lock = new SpinLock();
 
         static void Thread_1()
         {
             for (int i = 0; i < 1000000; i++)
             {
-                lock(_obj)
-                {
-                    number++;
-                }
+                _lock.Acquire();
+                _num++;
+                _lock.Release();
             }
         }
 
@@ -24,23 +50,22 @@ namespace ServerCore
         {
             for (int i = 0; i < 1000000; i++)
             {
-                lock (_obj)
-                {
-                    number--;
-                }
+                _lock.Acquire();
+                _num--;
+                _lock.Release();
             }
         }
 
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1); 
+            Task t1 = new Task(Thread_1);
             Task t2 = new Task(Thread_2);
-            t1.Start(); 
+            t1.Start();
             t2.Start();
 
             Task.WaitAll(t1, t2);
 
-            Console.WriteLine(number); 
+            Console.WriteLine(_num);
         }
     }
 }
