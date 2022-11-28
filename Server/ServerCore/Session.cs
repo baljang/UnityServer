39980366b02ebe72a294cbd48a9ecf07aa414a3d
baljang <baljang7@gmail.com>
2022-11-28 19:54:02 +0900

    02_14_네트워크 프로그래밍_PacketSession
    Q1. 게임 패킷을 설계 할 때 주로 넣는 두가지 요소는 무엇인가?
    
    Q2. sealed는 어떤 기능을 가지는가?
    
    Q3. 2바이트로 사이즈를 확인한 다음에 조립해서 넘겨주는 작업을 어떻게 했는지 설명하세요.

--- Server/ServerCore/Session.cs
+++ Server/ServerCore/Session.cs
@@ -7,8 +7,42 @@ using System.Threading;
 
 namespace ServerCore
 {
-    public abstract class Session
+    public abstract class PacketSession : Session
     {
+        public static readonly int HeaderSize = 2;
+
+        // [size(2)][packetId(2)[ ... ][size(2)][packetId(2)[ ... ]
+        public sealed override int OnRecv(ArraySegment<byte> buffer)
+        {
+            int processLen = 0;
+
+            while (true)
+            {
+                // 최소한 헤더는 파싱할 수 있는지 확인
+                if (buffer.Count < HeaderSize)
+                    break;
+
+                // 패킷이 완전체로 도착했는지 확인
+                ushort dataSize = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
+                if (buffer.Count < dataSize)
+                    break;
+
+                // 여기까지 왔으면 패킷 조립 가능
+                OnRecvPacket(new ArraySegment<byte>(buffer.Array, buffer.Offset, dataSize));  // 패킷이 해당하는 영역을 다시 찝어서 넘겨 줄거야
+
+                processLen += dataSize;
+                buffer = new ArraySegment<byte>(buffer.Array, buffer.Offset + dataSize, buffer.Count - dataSize); // 바뀐 애는 다음 부분을 찝어주는 애가 된다. 
+            }
+
+            return processLen;
+        }
+
+        public abstract void OnRecvPacket(ArraySegment<byte> buffer);
+
+    }
+
+    public abstract class Session
+    {      
         Socket _socket;
         int _disconnect = 0;
 
