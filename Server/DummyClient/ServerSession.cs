935ff8abb7bbf6cc85f82de7ff39b7f9bd419943
baljang <baljang7@gmail.com>
2022-12-07 22:49:32 +0900

    sln 파일 올라가는지 테스트
    테스트

--- Server/DummyClient/ServerSession.cs
+++ Server/DummyClient/ServerSession.cs
@@ -8,23 +8,50 @@ using ServerCore;
 
 namespace DummyClient
 {
-    class Packet
+    public abstract class Packet
     {
         public ushort size;
         public ushort packetId;
+
+        public abstract ArraySegment<byte> Write();
+        public abstract void Read(ArraySegment<byte> s); 
     }
     
     class PlayerInfoReq : Packet    // 클라에서 서버로 플레이어 정보를 알고 싶어
     {
-        public long playerId; 
+        public long playerId;
+
+        public PlayerInfoReq()
+        {
+            this.packetId = (ushort)PacketID.PlayerInfoReq; 
+        }
+
+        public override void Read(ArraySegment<byte> s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ArraySegment<byte> Write()
+        {
+            ArraySegment<byte> s = SendBufferHelper.Open(4096);
+
+            ushort count = 0; // 지금까지 몇 바이트를 버퍼에 밀어 넣었는지 추적할거야.
+            bool success = true;
+
+            // 혹시라도 중간에 한번이라도 실패하면 false가 뜬다.
+            //success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), packet.size);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.packetId);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.playerId);
+            count += 8;
+
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), count);
+
+            return SendBufferHelper.Close(count);
+        }
     }
 
-
-    class PlayerInfoOk : Packet     // 서버에서 클라로 답변을 주는 거 
-    {
-        public int hp;
-        public int attack;
-    }
 
     public enum PacketID
     {
